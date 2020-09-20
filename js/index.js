8c8fed28eee537a946e2b2a51f4e9cb1e1cc3248
jparedestrejo <47701158+jparedestrejo@users.noreply.github.com>
2020-09-20 00:13:18 -0500

    update

---
+++ js/index.js
@@ -0,0 +1,22 @@
+const daynames=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Monday"];
+const months=["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+function currentHour(){
+const todaysdate=new Date();
+const dayName= daynames[todaysdate.getDay()];
+const monthName= months[todaysdate.getMonth()];
+const currentdate=dayName+ ", "+todaysdate.getDate() +" "+ monthName+", "+ todaysdate.getFullYear();
+const hour=todaysdate.getHours();
+const minutes=todaysdate.getMinutes();
+const seconds=todaysdate.getSeconds();
+ if(seconds<10)
+currenthour=hour+":"+minutes+":0"+seconds;
+else
+currenthour=hour+":"+minutes+":"+seconds;
+if(minutes<10)
+currenthour=hour+":0"+minutes+":"+seconds;
+ else
+ currenthour=hour+":"+minutes+":"+seconds;
+document.getElementById("currentday").textContent="Last updated: "+currentdate+" - "+currenthour;
+t=setTimeout("currentHour()",1000);
+}
+currentHour();
