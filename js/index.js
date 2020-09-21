5e5967048427f07e71c675b70b5f93480002d84b
jparedestrejo <47701158+jparedestrejo@users.noreply.github.com>
2020-09-20 23:41:03 -0500

    commit

--- js/index.js
+++ js/index.js
@@ -5,18 +5,20 @@ const todaysdate=new Date();
 const dayName= daynames[todaysdate.getDay()];
 const monthName= months[todaysdate.getMonth()];
 const currentdate=dayName+ ", "+todaysdate.getDate() +" "+ monthName+", "+ todaysdate.getFullYear();
-const hour=todaysdate.getHours();
-const minutes=todaysdate.getMinutes();
-const seconds=todaysdate.getSeconds();
- if(seconds<10) 
-currenthour=hour+":"+minutes+":0"+seconds;
-else if(seconds<10)
-currenthour=hour+":"+minutes+":0"+seconds;
-else if(minutes<10)
-currenthour=hour+":"+minutes+":0"+seconds;
-else
+var hour=todaysdate.getHours();
+var minutes=todaysdate.getMinutes();
+var seconds=todaysdate.getSeconds();
+hour = check0(hour);
+minutes = check0(minutes);
+seconds = check0(seconds);
  currenthour=hour+":"+minutes+":"+seconds;
 document.getElementById("currentday").textContent="Last updated: "+currentdate+" - "+currenthour;
 t=setTimeout("currentHour()",1000);
+}
+function check0(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
 }
 currentHour();
