a33e81bcfb6fb1eddbf0ec887af656e46d7ef8d7
jparedestrejo <47701158+jparedestrejo@users.noreply.github.com>
2020-11-04 20:36:28 -0500

    commit

---
+++ lesson-8/js/index.js
@@ -0,0 +1,28 @@
+var daynames = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var t;
+function currentHour(){
+var todaysdate = new Date();
+var dayName = daynames[todaysdate.getDay()-1];
+var monthName = months[todaysdate.getMonth()];
+var currentdate = dayName+ ", "+todaysdate.getDate() +" "+ monthName+", "+ todaysdate.getFullYear();
+var hour = todaysdate.getHours();
+var minutes = todaysdate.getMinutes();
+var seconds = todaysdate.getSeconds();
+hour = check0(hour);
+minutes = check0(minutes);
+seconds = check0(seconds);
+ currenthour=hour+":"+minutes+":"+seconds;
+document.getElementById("lastupdated").innerHTML="Last Updated: "+currentdate+" - "+currenthour;
+document.getElementById("compiled").innerHTML=(todaysdate.getMonth()+1)+"."+(todaysdate.getDay()+1)+"."+todaysdate.getFullYear();
+
+//t=setTimeout("currentHour()",1000);
+}
+document.getElementById("copyright-year").innerText = new Date().getFullYear();
+function check0(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+currentHour();
