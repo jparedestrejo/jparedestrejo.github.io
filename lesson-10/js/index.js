7e3e47a677ed429fd3c892a2a010fea24165ec73
jparedestrejo <47701158+jparedestrejo@users.noreply.github.com>
2020-11-19 21:15:55 -0500

    commit

--- lesson-10/js/index.js
+++ lesson-10/js/index.js
@@ -14,7 +14,6 @@ minutes = check0(minutes);
 seconds = check0(seconds);
  currenthour=hour+":"+minutes+":"+seconds;
 document.getElementById("lastupdated").innerHTML="Last Updated: "+currentdate+" - "+currenthour;
-
 //t=setTimeout("currentHour()",1000);
 }
 document.getElementById("copyright-year").innerText = new Date().getFullYear();
@@ -25,73 +24,3 @@ function check0(i) {
     return i;
 }
 currentHour();
-/*-------------------*/
-const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
-
-fetch(requestURL)
-    .then(response => response.json())
-    .then(data => {
-        console.table(data);
-        const list = data['towns'];
-       const myTowns = list.filter(x=>(x.name=="Fish Haven"||x.name=="Preston"||x.name=="Soda Springs"));
-       console.table(myTowns);
-        myTowns.forEach(town => {
-            console.log(town.name);
-            let card = document.createElement('section');
-            let h2 = document.createElement('h2');
-            let h4 = document.createElement('h4');
-            let p1 = document.createElement('p');
-            let p2 = document.createElement('p');
-            let p3 = document.createElement('p');
-            let image = document.createElement('img');
-
-            let townInfo = document.createElement('div');
-            let townImage = document.createElement('div');
-           
-            h2.textContent = town.name;
-            h4.textContent = town.motto;
-            p1.textContent = "Year Founded: "+town.yearFounded;
-            p2.textContent = "Population: "+town.currentPopulation;
-            p3.textContent = "Annual Rain Fall: "+town.averageRainfall;
-            image.setAttribute('src', 'images/'+town.photo);
-            image.setAttribute('alt', town.name);
-
-            townInfo.appendChild(h2);
-            townInfo.appendChild(h4);
-            townInfo.appendChild(p1);
-            townInfo.appendChild(p2);
-            townInfo.appendChild(p3);
-            townImage.appendChild(image);
-
-            card.appendChild(townInfo);
-            card.appendChild(townImage);
-
-            document.querySelector('div.towns').appendChild(card);
-            
-        });
-
-    });
-  
-/*
- for (let i = 0; i < list.length; i++ ) {
-            let card = document.createElement('section');
-            let h2 = document.createElement('h2');
-            let p1 = document.createElement('p');
-            let p2 = document.createElement('p');
-            let image = document.createElement('img');
-
-            h2.textContent = list[i].name + ' ' + list[i].lastname;
-            p1.textContent = 'Date of Birth: ' + list[i].birthdate;
-            p2.textContent = 'Place of Birth: ' + list[i].birthplace;
-            image.setAttribute('src', list[i].imageurl);
-            image.setAttribute('alt', list[i].name + ' ' + list[i].lastname + " - " + list[i].order);
-
-            card.appendChild(h2);
-            card.appendChild(p1);
-            card.appendChild(p2);
-            card.appendChild(image);
-            document.querySelector('div.cards').appendChild(card);
-
-        }
-
-*/
