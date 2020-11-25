0ade3415b269599c737279580c047eb28e9b0a84
jparedestrejo <47701158+jparedestrejo@users.noreply.github.com>
2020-11-25 01:35:42 -0500

    commit

---
+++ lesson-11/js/preston-6.js
@@ -0,0 +1,13 @@
+
+
+
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.menu');
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+window.onresize = () => {if (window.innerWidth > 360) mainnav.classList.remove('responsive')};
+
+const d = new Date();
+const popup = document.querySelector(".pancakealert");
+
+if(d.getDay()==5) popup.style.display="block";
