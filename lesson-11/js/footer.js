c088a99ded3fdb220766ea871b35eaf152aa8ad6
jparedestrejo <47701158+jparedestrejo@users.noreply.github.com>
2020-11-30 01:38:16 -0500

    commit

--- lesson-11/js/footer.js
+++ lesson-11/js/footer.js
@@ -1,6 +1,6 @@
 var daynames = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-var t;
+//var t;
 function currentHour(){
 var todaysdate = new Date();
 var dayName = daynames[todaysdate.getDay()-1];
@@ -12,7 +12,7 @@ var seconds = todaysdate.getSeconds();
 hour = check0(hour);
 minutes = check0(minutes);
 seconds = check0(seconds);
- currenthour=hour+":"+minutes+":"+seconds;
+var currenthour=hour+":"+minutes+":"+seconds;
 document.getElementById("lastupdated").innerHTML="Last Updated: "+currentdate+" - "+currenthour;
 //t=setTimeout("currentHour()",1000);
 }
--- lesson-11/js/forecast.js
+++ lesson-11/js/forecast.js
@@ -1,5 +1,6 @@
 var dayweeknames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-const ntown = document.querySelector("#town_box h2").textContent;
+var ntown = document.querySelector("#town_box h2").textContent;
+var codi;
 if(ntown.includes("Preston"))
   codi="id=5604473";
 else if(ntown.includes("Soda"))
@@ -7,11 +8,11 @@ else if(ntown.includes("Soda"))
 else
   codi="lat=42.0380399&lon=-111.4048681";
 
-const apiURL="https://api.openweathermap.org/data/2.5/forecast?"+codi+"&units=imperial&appid=07e8cdd414f51e368d4433fb5d72ef74";
+var apiURL="https://api.openweathermap.org/data/2.5/forecast?"+codi+"&units=imperial&appid=07e8cdd414f51e368d4433fb5d72ef74";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    const list = jsObject['list'];
+    let list = jsObject['list'];
     let des = list[0].weather[0].description;
     let tempmax = list[0].main.temp_max;
     let humi = list[0].main.humidity;
@@ -21,13 +22,13 @@ fetch(apiURL)
     document.getElementById('humid').textContent = humi;
     document.getElementById('speed').textContent = winsp;
     console.log(jsObject);
-    const fivedays = jsObject.list.filter(x => x.dt_txt.includes("18:00:00"));
+    let fivedays = jsObject.list.filter(x => x.dt_txt.includes("18:00:00"));
     console.log(fivedays);
     let day=0;
     fivedays.forEach(element => {
-      const d = new Date(element.dt_txt);
+      let d = new Date(element.dt_txt);
       document.getElementById(`dayname${day+1}`).textContent = dayweeknames[d.getDay()];
-      const imagesrc = 'https://openweathermap.org/img/w/' + element.weather[0].icon + '.png';
+      let imagesrc = 'https://openweathermap.org/img/w/' + element.weather[0].icon + '.png';
       document.querySelectorAll(".weather-forecast ul li img")[day].src=imagesrc;
       document.querySelectorAll(".weather-forecast ul li p span")[day].textContent=Math.ceil(parseFloat(element.main.temp));
       day++;
