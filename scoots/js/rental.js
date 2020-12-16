cf3dc6b03e88b7c742f72881bef63ceb166a0055
jparedestrejo <47701158+jparedestrejo@users.noreply.github.com>
2020-12-16 17:09:35 -0500

    commiot

--- scoots/js/rental.js
+++ scoots/js/rental.js
@@ -60,7 +60,7 @@ const rentaltypes=[
 
 //console.log(rentaltypes[5].walkin[1]);
 
-const apiURL="https://jparedestrejo.github.io/scoots/data/rental.json";
+const apiURL="https://jparedestrejo.github.io/data/rental.json";
 fetch(apiURL)
   .then((response) => response.json())
   
