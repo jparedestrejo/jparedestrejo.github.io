2666fa5e252c32d03b8c94ff4209f90bdba3dbd8
jparedestrejo <47701158+jparedestrejo@users.noreply.github.com>
2020-12-16 19:37:52 -0500

    commit

--- scoots/js/rental.js
+++ scoots/js/rental.js
@@ -6,10 +6,44 @@ fetch(apiURL)
   .then((response) => response.json())
   .then((data) => {
     
-    console.table(data)
     const list = data['typesrental'];
-    console.table(list);
+    list.forEach(town => {
+      let card = document.createElement('section');
+      let h2 = document.createElement('h2');
+      let h4 = document.createElement('h4');
+      let p1 = document.createElement('p');
+      let p2 = document.createElement('p');
+      let p3 = document.createElement('p');
+      let p4 = document.createElement('p');
+      let p5 = document.createElement('p');
+      let image = document.createElement('img');
+
+      let townInfo = document.createElement('div');
+      let townImage = document.createElement('div');
+     
+      h2.textContent = town.type;
+      h4.textContent = town.description;
+      p1.textContent = "Max. per: "+town.maxper;
+      p2.textContent = "Reservation Half Day: "+town.reservation[0];
+      p3.textContent = "Reservation Full Day: "+town.reservation[1];
+      p4.textContent = "Walk-in Half Day: "+town.walkin[0];
+      p5.textContent = "Walk-in Full Day: "+town.walkin[1];
+      image.setAttribute('src', 'images/rental/'+town.url);
+      image.setAttribute('alt', town.description);
+
+      townInfo.appendChild(h2);
+      townInfo.appendChild(h4);
+      townInfo.appendChild(p1);
+      townInfo.appendChild(p2);
+      townInfo.appendChild(p3);
+      townInfo.appendChild(p4);
+      townInfo.appendChild(p5);
+      townImage.appendChild(image);
+
+      card.appendChild(townInfo);
+      card.appendChild(townImage);
+
+      document.querySelector('div#rent').appendChild(card);
+
+    });
 });
-
-
-//        myTowns.forEach(town => {
