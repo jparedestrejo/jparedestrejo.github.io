0366c3cb29247a7e299d9b1d077ad9fcc1bb63e8
jparedestrejo <47701158+jparedestrejo@users.noreply.github.com>
2020-12-17 23:22:52 -0500

    commit

--- scoots/js/weathersummary.js
+++ scoots/js/weathersummary.js
@@ -11,9 +11,9 @@ fetch(apiURL)
     let humi = list[0].main.humidity;
     let winsp = list[0].wind.speed;
     document.getElementById('desc').textContent = des;
-    document.getElementById('temp').textContent = tempmax;
-    document.getElementById('humid').textContent = humi;
-    document.getElementById('speed').textContent = winsp;
+    document.getElementById('temp').textContent = Math.round(tempmax);
+    document.getElementById('humid').textContent = humi+"%";
+    document.getElementById('speed').textContent = Math.round(winsp)+" mph";
     console.log(jsObject);
     const threedays = jsObject.list.filter(x => x.dt_txt.includes("18:00:00"));
     console.log(threedays);
@@ -24,7 +24,8 @@ fetch(apiURL)
       const imagesrc = 'https://openweathermap.org/img/w/' + element.weather[0].icon + '.png';
       document.querySelectorAll(".weather-forecast ul li img")[day].src=imagesrc;
       document.querySelectorAll(".weather-forecast ul li img")[day].setAttribute("alt",element.weather[0].icon + '.png');
-      document.querySelectorAll(".weather-forecast ul li p span")[day].textContent=element.main.temp;
+      document.querySelectorAll(".weather-forecast ul li p span")[day].textContent=Math.round(element.main.temp);
       day++;
     });
   });
+  
